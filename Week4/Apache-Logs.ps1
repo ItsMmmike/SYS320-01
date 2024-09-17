206fd577915a8e44b78f6d89ece8bdcb8f92df66
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2024-09-17 01:28:00 -0400

    Add files via upload

---
+++ Week4/Apache-Logs.ps1
@@ -0,0 +1,27 @@
+﻿# Function used to take 3 inputs (Page visited, HTTP code returned, Name of Web Browser)
+# and returns a filter of all IP Addresses that accessed the webpage using the input vars
+
+Function FilterRequestIP ([string]$pageVisited,[string]$codeReturned, [string]$Browser) {
+    
+    # Retrieves all apache logs that contain the specified information (webpage, HTTP Status Code, Browser Type)
+    $query = Get-Content C:\xampp\apache\logs\access.log | Select-String $pageVisited | Select-String $codeReturned | Select-String $Browser
+
+    # Define a regex for IP address
+    $regex = [regex] "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+
+    # Get $query records that match to the regex
+    $ipsUnorganized = $regex.Matches($query)
+
+    # Get ips as pscustonboject
+    $ips = @()
+    for($i=0; $i -lt $ipsUnorganized.Count; $i++){
+     $ips += [pscustomobject]@{ "IP" = $ipsUnorganized[$i].Value}
+    }
+
+    # Filter IPs within the 10.X.X.X range
+    $ipsoftens = $ips | Where-Object { $_.IP -ilike "10.*" }
+    $counts = $ipsoftens | Group IP
+
+    # Function outputs a table containing IPs matching the search query + count number
+    return $counts | Select-Object Count, Name
+}
---
+++ Week4/main.ps1
@@ -0,0 +1,6 @@
+﻿# Script that calls/runs functions from the Apache-Logs.ps1 file via dot notation
+. (Join-Path $PSScriptRoot .\'Apache-Logs.ps1')
+
+# Runs funciton from Apache-Logs.ps1 using the specified parameters
+$Result = FilterRequestIP "index.html" "404" "Chrome"
+$Result
