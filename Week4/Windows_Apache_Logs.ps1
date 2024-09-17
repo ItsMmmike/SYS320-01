3ed938ffef0d8325e7dcd3c613dbaeea3aaad7e7
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2024-09-16 23:25:43 -0400

    Update Windows_Apache_Logs.ps1

--- Week4/Windows_Apache_Logs.ps1
+++ Week4/Windows_Apache_Logs.ps1
@@ -1,1 +1,36 @@
-# a
+# Q2.) List all of the apache logs of xampp
+Get-Content C:\xampp\apache\logs\access.log
+
+# Q3.) Script used to list the last 5 apache logs of xampp
+Get-Content C:\xampp\apache\logs\access.log -Tail 5
+
+# Q4.) Display only logs that contain 404 (Not Found) or 400 (Bad Request)
+Get-Content C:\xampp\apache\logs\access.log | Select-String ' 404 ',' 400 '
+
+# Q5.) Display only logs that contain 404 (Not Found) or 400 (Bad Request)
+Get-Content C:\xampp\apache\logs\access.log | Select-String ' 200 ' -NotMatch
+
+# Q6.) From every .log file in the directory, only get logs that contains the word 'error'
+$A = Get-ChildItem -Path C:\xampp\apache\logs\*.log | Select-String 'error'
+$A[-5..-1]
+
+# Q7.) Get only logs that contain 404, save into $notfounds
+$notfounds = Get-Content C:\xampp\apache\logs\access.log | Select-String ' 404 '
+
+# Define a regex for IP address
+$regex = [regex] "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
+
+# Get $notfounds recrods that match to the regex
+$ipsUnorganized = $regex.Matches($notfounds)
+
+# Get ips as pscustonboject
+$ips = @()
+for($i=0; $i -lt $ipsUnorganized.Count; $i++){
+ $ips += [pscustomobject]@{ "IP" = $ipsUnorganized[$i].Value}
+}
+
+# Q8.) Similar script to Q7, except we cound the number of IPs from the output 
+#      (**Note: This is also using the previous script above)
+$ipsoftens = $ips | Where-Object { $_.IP -ilike "10.*" }
+$counts = $ipsoftens | Group IP
+$counts | Select-Object Count, Name
