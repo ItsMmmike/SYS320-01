40b1a1443d897e9fb819ae495b9a99b5d3b68c49
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2024-09-22 02:29:57 -0400

    Update main.ps1

--- Week4/main.ps1
+++ Week4/main.ps1
@@ -1,6 +1,14 @@
-﻿# Script that calls/runs functions from the Apache-Logs.ps1 file via dot notation
+<#
+# Script that calls/runs functions from the Apache-Logs.ps1 file via dot notation
 . (Join-Path $PSScriptRoot .\'Apache-Logs.ps1')
 
 # Runs funciton from Apache-Logs.ps1 using the specified parameters
 $Result = FilterRequestIP "index.html" "404" "Chrome"
 $Result
+#>
+
+# Script that calls/runs the "ApacheLogs1" function from the "Parsing_Apache_Logs.ps1" file
+. (Join-Path $PSScriptRoot .\'Parsing_Apache_Logs.ps1')
+
+# Runs the called function and outputs the results
+ApacheLogs1
