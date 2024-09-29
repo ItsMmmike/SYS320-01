6ed361c62ca2397890b0ca32ae32d44ffca2dd27
ItsMmmike <113321364+ItsMmmike@users.noreply.github.com>
2024-09-29 18:05:11 -0400

    Add files via upload

---
+++ Week5/Scraping_Champlain_Classes.ps1
@@ -0,0 +1,66 @@
+﻿# Function used to convert the existing "Days" output to days of the week
+function daysTranslator($FullTable){
+
+# Go over every record in the table
+for($i=0; $i -lt $FullTable.length; $i++){
+
+    # Empty array to hold days for every record
+    $Days = @()
+
+    # If you see "M" -> Monday
+    if($FullTable[$i].Days -ilike "M*"){ $Days += "Monday"}
+
+    # If you see "T" followed by T,W, or F -> Tuesday
+    if($FullTable[$i].Days -ilike "*[TWF]*"){ $Days += "Tuesday"}
+    # If you only see "T" -> Tuesday
+    ElseIf($FullTable[$i].Days -ilike "T"){ $Days += "Tuesday"}
+
+    # If you see "W" -> Wednesday
+    if($FullTable[$i].Days -ilike "W"){ $Days += "Wednesday"}
+
+    # If you see "TH" -> Thursday
+    if($FullTable[$i].Days -ilike "*TH*"){ $Days += "Thrusday"}
+
+    # F -> Friday
+    if($FullTable[$i].Days -ilike "*F"){ $Days += "Friday"}
+
+    # Make the switch
+    $FullTable[$i].Days = $Days
+
+}
+
+return $FullTable
+}
+
+# Function used to webscrape Champlain College class data from a webpage and formats as table
+function gatherClasses(){
+
+$page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.18/courses-1.html
+
+# Get all the tr elements of HTML document
+$trs=$page.ParsedHtml.body.getElementsByTagName("tr")
+
+# Empty array to hold results
+$fullTable = @()
+for($i=1; $i -lt $trs.length; $i++){ # Going over every tr element
+
+    # Get every td element of current tr element
+    $tds = $trs[$i].getElementsbyTagName("td")
+
+    # Want to separate start time and end time from one time field
+    $Times = $tds[5].innerText.split("-")
+
+    $FullTable += [pscustomobject]@{"Class Code"  = $tds[0].innerText;
+                                     "Title"      = $tds[1].outerText;
+                                     "Days"       = $tds[4].innerText;
+                                     "Time Start" = $Times[0];
+                                     "Time End"   = $Times[1];
+                                     "Instructor" = $tds[6].outerText;
+                                     "Location"   = $tds[9].innerText;
+                                }
+
+} # end of for loop
+
+# Run results through days translator function
+daysTranslator $FullTable
+}
---
+++ Week5/main.ps1
@@ -0,0 +1,31 @@
+﻿# Script that calls/runs the "gatherClasses" function from the "Scraping_Champlain_Classes.ps1" file
+. (Join-Path $PSScriptRoot .\'Scraping_Champlain_Classes.ps1')
+
+# Runs the function "gatherClasses" and outputs the results into the "$FullTable" variable
+$FullTable = gatherClasses
+
+# Q1.) List all the classes of Instructor Furkan Paligu
+#$FullTable | Select-Object "Class Code", Instructor, Location, Days, "Time Start", "Time End" |
+#             Where { $_."Instructor" -ilike "Furkan*" }
+
+# Q2.) List all the classes of JOYC 310 on Mondays, only display Class Code and Times
+# Sort by Start Time
+#$FullTable | Where-Object { ($_.Location -ilike "JOYC 310") -and ($_.days -contains "Monday") } |
+#             Sort-Object "Time Start" |
+#             Format-Table "Time Start", "Time End", "Class Code"
+
+# Q3.) Make a list of all the instructors that teach at least 1 course in
+# SYS, SEC, NET, FOR, CSI, DAT
+# Sort by name, and make it unique
+$ITSInstructors = $FullTable | Where-Object { ($_."Class Code" -ilike "SYS*") -or `
+                                              ($_."Class Code" -ilike "SEC*") -or `
+                                              ($_."Class Code" -ilike "NET*") -or `
+                                              ($_."Class Code" -ilike "FOR*") -or `
+                                              ($_."Class Code" -ilike "CSI*") -or `
+                                              ($_."Class Code" -ilike "DAT*") } `
+#                             | Select-Object "Instructor" `
+#                             | Sort-Object "Instructor" -Unique
+
+# Q4.) Group all the instructors by the number of classes they are teaching (Uses the query script from the prev question)
+$FullTable | Where { $_.Instructor -in $ITSInstructors.Instructor } `
+           | Group-Object "Instructor" | Select-Object Count,Name | Sort-Object Count -Descending
